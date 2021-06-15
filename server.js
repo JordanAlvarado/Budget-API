545543111189c67722edc4b6dba73c8a8ba997c9
Jordan Alvarado <jordan.alvarado@me.com>
2021-06-15 17:04:11 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+//Dependencies
+
+const app = require("./app.js")
+
+require("dotenv").config();
+
+//Port
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3003;
+
+// Routes
+
+//Listen
+app.listen(PORT, () => console.log("Listening on port:", PORT));
